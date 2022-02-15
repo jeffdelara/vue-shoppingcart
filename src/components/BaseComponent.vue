bb37996f65497b46c0d7ce69735214532129db5d
Angelo Berina <angelo@gorated.ph>
2022-02-15 15:24:12 +0800

    initial commit

---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="base-component">
+    Sample Component Initialization, <br />
+    omit this file if you started working on your project 🤞
+
+    <div>Clicks: {{ counter }}</div>
+
+    <button v-on:click="addCount">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseComponent",
+  data: () => ({
+    counter: 0,
+  }),
+  methods: {
+    addCount() {
+      this.counter += 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="../styles/components/_base-component.scss" />
